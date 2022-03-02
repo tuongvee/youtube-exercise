d251817a5aa7aafaff4b54032abe0a85971c1c4e
tuongvee <18520403@gm.uit.edu.vn>
2022-03-03 05:09:08 +0700

    sidemenu, video section

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const btnScrollLeft = document.querySelector(".scroll-left-btn");
 const btnScrollRight = document.querySelector(".scroll-right-btn");
 const btnSideMenu = document.querySelector(".nav-btn");
+const sideMenu = document.getElementById("sidemenu");
 
-btnSideMenu.addEventListener("click", function sideMenu() {
-  btnSideMenu.classList.toggle(open);
+btnSideMenu.addEventListener("click", function () {
+  if (!sideMenu.classList.contains("open")) {
+    sideMenu.classList.toggle("open");
+    sideMenu.classList.toggle("close");
+    document.querySelector(".sidemenu-collapse").style.display = "none";
+  } else {
+    sideMenu.classList.toggle("open");
+    sideMenu.classList.toggle("close");
+    document.querySelector(".sidemenu-collapse").style.display = "block";
+  }
 });
 
 function slide(direction) {
@@ -11,15 +20,15 @@ function slide(direction) {
   scrollCompleted = 0;
   var slideVar = setInterval(function () {
     if (direction == "left") {
-      categoriesSection.scrollLeft -= 10;
+      categoriesSection.scrollLeft -= 50;
     } else {
       document.querySelector(".scroll-left-btn-backgound").style.display =
         "inline-flex";
       btnScrollLeft.style.display = "inline-flex";
-      categoriesSection.scrollLeft += 10;
+      categoriesSection.scrollLeft += 50;
     }
-    scrollCompleted += 10;
-    if (scrollCompleted >= 100) {
+    scrollCompleted += 50;
+    if (scrollCompleted >= 500) {
       window.clearInterval(slideVar);
     }
   }, 50);
