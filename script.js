55374948a6d1b7e7bfd99f009a040842f69e4a73
tuongvee <18520403@gm.uit.edu.vn>
2022-03-03 10:28:24 +0700

    sticky header, video section

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ const btnScrollLeft = document.querySelector(".scroll-left-btn");
 const btnScrollRight = document.querySelector(".scroll-right-btn");
 const btnSideMenu = document.querySelector(".nav-btn");
 const sideMenu = document.getElementById("sidemenu");
+const video = document.querySelector(".video");
 
 btnSideMenu.addEventListener("click", function () {
   if (!sideMenu.classList.contains("open")) {
     sideMenu.classList.toggle("open");
     sideMenu.classList.toggle("close");
     document.querySelector(".sidemenu-collapse").style.display = "none";
+    document.querySelector(".categories").style.marginLeft = "22rem";
+    document.querySelector(".videos").style.marginLeft = "22rem";
   } else {
     sideMenu.classList.toggle("open");
     sideMenu.classList.toggle("close");
     document.querySelector(".sidemenu-collapse").style.display = "block";
+    document.querySelector(".categories").style.marginLeft = "8rem";
+    document.querySelector(".videos").style.marginLeft = "8rem";
   }
 });
 
@@ -33,3 +38,17 @@ function slide(direction) {
     }
   }, 50);
 }
+
+video.addEventListener("click", function () {
+  let url = video.getAttribute("data-link");
+  console.log(url);
+  var myWindow = window.open("", "_self");
+  myWindow.document.write(`
+  <!DOCTYPE html>
+  <html>
+  <body>
+  <iframe width="100%" height="100%" src=""></iframe>
+  </body>
+  </html>
+  `);
+});
